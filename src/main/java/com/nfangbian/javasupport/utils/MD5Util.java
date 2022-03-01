d0e83219db498a09d08cdddc2b44f97b5acbfd7e
程金盛 <jinsheng.cheng@handeson.com>
2022-03-01 17:40:28 +0800

    add func

---
+++ src/main/java/com/nfangbian/javasupport/utils/MD5Util.java
@@ -0,0 +1,61 @@
+package com.nfangbian.javasupport.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+
+    // 小写md5字符串
+    public static String Md5V1(String strSrc) {
+        MessageDigest md;
+        byte[] digest = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+            byte[] bytes = strSrc.getBytes("UTF-8");
+            md.update(bytes);
+            digest = md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < digest.length; i++){
+            int val = ((int) digest[i]) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+
+        return hexValue.toString();
+    }
+
+    // 大写md5字符串
+    public static String Md5V2(String strSrc) {
+        char hexDigits[] = {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+        };
+        try {
+            byte[] btInput = strSrc.getBytes();
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+}
